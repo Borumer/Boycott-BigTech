8cddabab07f05d28dd4afd85f4190c9e6dc6f03e
Varun <31084854+Borumer@users.noreply.github.com>
2021-02-19 22:12:34 -0500

    Prepare Navbar, start README

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Facebook from "./Facebook/facebook";
+import Google from "./Google/google";
+import Amazon from "./Amazon/amazon";
+import Apple from "./Apple/apple";
+import Home from "./Home/home";
+import Join from "./Join/join";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Boycott Big Tech</Link>
+            </li>
+            <li>
+              <Link to="/Facebook">Facebook</Link>
+            </li>
+            <li>
+              <Link to="/Google">Google</Link>
+            </li>
+            <li>
+              <Link to="/Apple">Apple</Link>
+            </li>
+            <li>
+              <Link to="/Amazon">Amazon</Link>
+            </li>
+            <li>
+              <Link to="/join">Join Us</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/Facebook">
+            <Facebook />
+          </Route>
+          <Route path="/Google">
+            <Google />
+          </Route>
+          <Route path="/Apple">
+            <Apple />
+          </Route>
+          <Route path="/Amazon">
+            <Amazon />
+          </Route>
+          <Route path="/join">
+            <Join />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Apple/apple.js
@@ -0,0 +1,7 @@
+export default function Apple(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
